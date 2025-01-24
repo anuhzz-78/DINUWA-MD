999bee4876de1115664a7792b88194c94d18a75a
Dinuwa-md <dinuwamdnew2007@gmail.com>
2025-01-24 18:06:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,6 +23,6 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺ᴍʀ ᴅɪɴᴜᴡᴀ࿐",
+    BOT_NAME: process.env.BOT_NAME || "WHITE VIPER",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
